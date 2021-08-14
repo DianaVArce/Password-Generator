b61552217135b6a397b6c7b43e0569cb268b6bc8
DianaVArce <60335621+DianaVArce@users.noreply.github.com>
2021-08-14 12:24:12 -0700

    Finished Generating Arrays
    Created arrays for possible passwords and user options.
    Created final password array to create final password generated
    Added comments to code for better descriptions.

--- Assets/script.js
+++ Assets/script.js
@@ -11,56 +11,105 @@ var upperCaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"
 var lowerCaseCharacters = ["a","b","c","d","e","f","g","h","i","j",
 "k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
+
 //Function to declare all the user's password preferences
 function userPasswordChoices(){
   var userLength = prompt("How many characters would you like your password to contain: ");
 
-  //Confirming that user entered a valid number for password generation
+  //Confirming that user entered a valid number for password generator
   if(isNaN(userLength)){
     alert("Please enter a valid number between 8 and 129.");
+    
   }
-  else{
-    if(userLength < 8){
-      alert("Password length must be at least 8 characters.");
-      return;      
-    }
-    else if(userLength > 129){
-      alert("Password length must be less than 129 characters.")
-      return;
-    }
+ 
+  //If valid number entered, check if the length is valid for generator
+  if(userLength < 8){
+    alert("Password length must be at least 8 characters.");
+    return;      
+  }
+  else if(userLength > 129){
+    alert("Password length must be less than 129 characters.")
+    return;
+  }
 
-    //Confirmation of type of characters to use to generate password
-    var specialCharaConfirm = confirm("Click OK to confirm including special characters");
-    var numeralCharaConfirm = confirm("Click OK to confirm including numeric characters");
-    var upperCaseCharaConfirm = confirm("Click OK to confirm including uppercase characters");
-    var lowerCaseCharaConfirm = confirm("Click OK to confirm including lowercase characters");
+  //Confirmation of type of characters to use to generate password
+  var specialCharaConfirm = confirm("Click OK to confirm including special characters");
+  var numeralCharaConfirm = confirm("Click OK to confirm including numeric characters");
+  var upperCaseCharaConfirm = confirm("Click OK to confirm including uppercase characters");
+  var lowerCaseCharaConfirm = confirm("Click OK to confirm including lowercase characters");
     
-    //Condition statement in case user did not select any character type
-    if(!specialCharaConfirm && !numeralCharaConfirm && !upperCaseCharaConfirm && !lowerCaseCharaConfirm){
-      alert("Must select at least one character type."); 
-    }
-    else{
-      alert("LEVEL CLEAR!");
-    }
+  //Condition statement in case user did not select any character type
+  if(!specialCharaConfirm && !numeralCharaConfirm && !upperCaseCharaConfirm && !lowerCaseCharaConfirm){
+    alert("Must select at least one character type."); 
+    return;
+  }
     
+  //Object created to store user's password confirmations
+  var userOptions = {
+    userlength: userLength,
+    specialChar: specialCharaConfirm,
+    numeralChar: numeralCharaConfirm,
+    upperCaseChar: upperCaseCharaConfirm,
+    lowerCaseChar: lowerCaseCharaConfirm
   };
-
+  
+  //Return created user-input object
+  return userOptions;
+  
 };
 
+//Function to generate password given all the user's choices
+function generatePassword() {
+  var passwordOptions = userPasswordChoices();   //Acces the object created in previous function and create arrays to hold passwords
+  var passwordArray = []; //array for random password created with given lists
+  var userGivenChoices = []; //array for all possible user combonations choices
 
+  //If user declares they want special characters in their password, add to possible choice array at random
+  if (passwordOptions.specialChar) {
+    userGivenChoices = userGivenChoices.concat(specialCharacters);
+    passwordArray.push(specialCharacters[Math.floor(Math.random()*specialCharacters.length)]);
+  }
 
+  //If user declares they want numerical characters in their password, add to possible choice array at random
+  if (passwordOptions.numeralChar) {
+    userGivenChoices = userGivenChoices.concat(numericalCharacters);
+    passwordArray.push(numericalCharacters[Math.floor(Math.random()*numericalCharacters.length)]);
+  }
 
+  //If user declares they want uppercase characters in their password, add to possible choice array at random
+  if (passwordOptions.upperCaseChar) {
+    userGivenChoices = userGivenChoices.concat(upperCaseCharacters);
+    passwordArray.push(upperCaseCharacters[Math.floor(Math.random()*upperCaseCharacters.length)]);
+  }
 
+  //If user declares they want lowercase characters in their password, add to possible choice array at random
+  if (passwordOptions.lowerCaseChar) {
+    userGivenChoices = userGivenChoices.concat(lowerCaseCharacters);
+    passwordArray.push(lowerCaseCharacters[Math.floor(Math.random()*lowerCaseCharacters.length)]);
 
+  }
 
+  //For loop to add the randomized user password choices to the array of possible generated passwords
+  for (i = passwordArray.length; i < passwordOptions.userlength; i++) {
+    passwordArray.push (userGivenChoices[Math.floor(Math.random() * userGivenChoices.length)]);
+  }
+  
+  //Convert the possible generated password array to final password string
+  var finalPwd = "";
+  for(var i = 0; i < passwordOptions.userlength; i++) {
+    finalPwd = finalPwd + passwordArray[Math.floor(Math.random() * passwordArray.length)]
+  
+  }
 
-
-
+ // console.log(finalPwd);
+  return finalPwd;
+ 
+};
 
 
 // Write password to the #password input
 function writePassword() {
-  var password = userPasswordChoices();
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
